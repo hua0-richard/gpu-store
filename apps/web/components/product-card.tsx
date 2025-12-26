3ff0e5a76c63938e55a2c14203dd98bfad517733
Richard Hua <hua.richard0@gmail.com>
2025-12-26 15:02:30 -0500

    Initial Commit

---
+++ apps/web/components/product-card.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export function ProductCard() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+        <CardAction>Card Action</CardAction>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+}
